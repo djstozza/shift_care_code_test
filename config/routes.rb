1f0b0bb03f570632d0eae137c95c45efb561a57a
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2021-10-28 08:19:25 +1100

    Add typescript

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,7 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  # Catch all for HTML 5 history routing. This must be the last route.
+  get '/*path', to: 'application#index', format: false
 end
